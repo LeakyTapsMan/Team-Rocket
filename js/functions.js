f572d2f2bc66a19c96093169154e106b77cfd61c
LeakyTapsMan <Conal@silkmothltd.local>
2016-10-19 08:48:39 +0100

    Updated err thing

--- js/functions.js
+++ js/functions.js
@@ -13,13 +13,26 @@ $(function() {
 
 $(window).scroll(function () {
 
-    /* Check the location of each desired element */
+    // Header expand/shrink on scroll
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+        shrinkOn = 300,
+        header = $(".header");
+
+    if (distanceY > shrinkOn) {
+        header.addClass("smaller");
+    } else {
+        if (header.hasClass("smaller")) {
+            header.removeClass("smaller");
+        }
+    }
+
+    // Add fade in transition for elements
     $('.fadeIn').each(function (i) {
 
         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
         var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-        /* If the object is completely visible in the window, fade it it */
+        // If the object is completely visible in the window, fade it
         if (bottom_of_window > bottom_of_object) {
 
             $(this).animate({
@@ -28,4 +41,5 @@ $(window).scroll(function () {
 
          }
      });
+
 });
